35158f969ccefcd7c987f87ef0ea390006389956
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-05-21 12:38:28 +0200

    facturas prueba

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,12 +1,12 @@
 const {postCreateUser, getUserID, getUsers, UserDeleteId, updateUserId}=require("./userController");
 const { login, validToken, pruebaBBDD } = require("./loginController");
 const {tokenValid, tokenValidAdmin} = require("./authController");
-const { borrarUsuarios, borrarInvoices } = require("./bdController");
+const { borrarUsuarios, borrarInvoices, createUsers, createInvoices } = require("./bdController");
 
 
 module.exports = {
     postCreateUser, getUserID, getUsers, UserDeleteId, updateUserId,
     login, validToken, pruebaBBDD,
     tokenValid, tokenValidAdmin,
-    borrarUsuarios, borrarInvoices
+    borrarUsuarios, borrarInvoices, createUsers, createInvoices
 }
